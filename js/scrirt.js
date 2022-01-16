c7a4530ec0101fedec5d6fe4b269f77a5d7b281e
Denis <unlabel@yandex.ru>
2022-01-16 18:30:39 +0300

    Запись ответов в объект movies

--- js/scrirt.js
+++ js/scrirt.js
@@ -4,8 +4,18 @@ console.log(numberOfFilms);
 const personalMovieDB = {
 
     count: numberOfFilms,  
-    movies: [],
-    actors: [],
+    movies: {},
+    actors: {},
     genres: [],
     privat: false   
 };
+
+const a = prompt("Предпоследний просмотренный фильм?", ""),
+      b = prompt("Оцените фильм от 0 до 10", ""),
+      c = prompt("Последний просмотренный фильм?", ""),
+      d = prompt("Оцените фильм от 0 до 10", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
